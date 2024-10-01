7752e8b5e8c23e176eace0a72741c9159850767c
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-10-01 11:30:29 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,21 +12,33 @@ closeBtn.addEventListener("click", () => {
   sidenav.style = "left: -100%";
 });
 /****************************************************** */
-let h1 = document.getElementsByTagName("h1")[0];
 let fond_accueil = document.querySelector(".fond_accueil");
 fond_accueil.addEventListener("mouseover", () => {
   fond_accueil.classList.add("new_font");
-  h1.innerHTML = "profitez de la qualité des service";
 });
 fond_accueil.addEventListener("mouseout", () => {
   fond_accueil.classList.remove("new_font");
-  h1.innerHTML = "HIGH QUALITY TECH VETEMENTS & ACCESSOIRES";
 });
-
 /****************************************************** */
 let addToCard = document.querySelectorAll(".add");
 for (let i = 0; i < addToCard.length; i++) {
   addToCard[i].href = "formulaire.html";
-  addToCard[i].title="formulaire d'achat"
+  addToCard[i].title = "formulaire d'achat";
+  addToCard[i].innerHTML = "commander";
 }
-
+/************************ */
+function fidelite() {
+  alert(
+    "Devenez membre du programme fidelité \n Un chèque de fidelité de 10% du total des 5 articles achetés"
+  );
+}
+/****************************section div********************* */
+let boxArticle = document.querySelectorAll(".boxArticle");
+for (let z = 0; z < boxArticle.length; z++) {
+  boxArticle[z].addEventListener("mouseover", () => {
+    boxArticle[z].classList.add("boxClass");
+  });
+  boxArticle[z].addEventListener("mouseout", () => {
+    boxArticle[z].classList.remove("boxClass");
+  });
+}
